2f802688bae6b946e0457dc2eabb524c00ba9c02
RakonaDev <juancajas1905@gmail.com>
2025-01-03 12:06:58 -0500

    a

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
+
+/** @type { import('next').NextConfig } */ 
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
