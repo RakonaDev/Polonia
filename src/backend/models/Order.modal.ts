8ed4b465224c36198971819a6278ae3f13b2bbb1
RakonaDev <juancajas1905@gmail.com>
2025-01-05 22:42:18 -0500

    f

--- src/backend/models/Order.modal.ts
+++ src/backend/models/Order.modal.ts
@@ -4,7 +4,7 @@ import { UserShop } from "./Users.modal"
 export interface Order {
   id: string
   details: DetailOrder[]
-  userOrdered: Pick<UserShop, 'uid' | 'name' | 'email'>
+  userOrdered: Pick<UserShop, 'id' | 'username' | 'email'>
   total: number
   status: string
   createdAt: Date
