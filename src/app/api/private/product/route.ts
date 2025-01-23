e831e48a0642aa96a63f15217d9bf229f7cade03
RakonaDev <juancajas1905@gmail.com>
2025-01-23 07:37:56 -0500

    dd

--- src/app/api/private/product/route.ts
+++ src/app/api/private/product/route.ts
@@ -45,7 +45,7 @@ export async function GET(req: NextRequest) {
 
 export async function POST(req: NextRequest) {
   const development = process.env.NEXT_PUBLIC_DEVELOPMENT
-  const root = development !== 'development' ? 'public/images/' : process.cwd() + 'assets/'
+  const root = development !== 'development' ? '/public/images/' : process.cwd() + 'assets/'
   try {
     const data: FormData = await req.formData();
 
